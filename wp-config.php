5f59416a1ea8fd83a71e9496fece00ab682c5ca2
sev7n <root@sev7n.net>
2011-07-10 12:46:24 +0800

    按钮 首页滚动按钮点不上BUG

--- wp-config.php
+++ wp-config.php
@@ -21,7 +21,7 @@ define('DB_NAME', 'damrey');
 define('DB_USER', 'root');
 
 /** MySQL 数据库密码 */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'softdons');
 
 /** MySQL 主机 */
 define('DB_HOST', 'localhost');
