c9552789199c1e1619ee136b083087ad2af51600
mo <david_zdd@hotmail.com>
2011-08-15 14:47:26 +0800

    add 其他域名的调整

--- index.php
+++ index.php
@@ -11,9 +11,25 @@
  *
  * @var bool
  */
+
+//add by mo at 2011-08-15 针对其他域名做跳转
+$redirestHostArr = array('www.hzbq.com.cn', 'hzbq.com.cn', 'www.bqba.com.cn', 'bqba.com.cn');
+if(in_array($_SERVER['HTTP_HOST'], $redirestHostArr)){
+	$uri = $_SERVER['REQUEST_URI'];
+	$pos = strpos($uri, '/index.php');
+	if($pos==0){
+		$uri = substr($uri, 10);
+	}
+	Header("HTTP/1.1 301 Moved Permanently");
+	Header("Location: http://www.hzcopyright.com{$uri}");
+	exit;
+}
+
 define('WP_USE_THEMES', true);
+
 //add by mo at 2011-07-07
 session_start();
+
 
 /** Loads the WordPress Environment and Template */
 require('./wp-blog-header.php');
